c426fd32f5a61431d1aed3d97fb1cf71319e8d00
Pelayo-Reguera <UO282348@uniovi.es>
2024-03-10 23:37:38 +0100

    User and MapFragment docs

--- App/app/src/main/java/com/example/mycity/logic/dataclasses/User.kt
+++ App/app/src/main/java/com/example/mycity/logic/dataclasses/User.kt
@@ -8,10 +8,12 @@ package com.example.mycity.logic.dataclasses
  * @property likedEvents A [MutableList] containing all the events that the user like or is register
  * as an attendant
  * @property coins A [MutableList] containing the quantities of each coin type in the user's possession
+ *
+ * @author Pelayo Reguera García
  * */
 data class User(
     val id: String,
     val userName: String,
     val likedEvents: MutableList<Event>,
-    val coins: MutableList<Int>
+    val coins: MutableMap<String, Int>
 ) {}
