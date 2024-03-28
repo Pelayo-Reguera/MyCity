7839758ade4abf5dffb26e0fc5f2f5f63c099c2d
Pelayo-Reguera <UO282348@uniovi.es>
2024-03-28 21:50:47 +0100

    Basic implementation of the map fragment

--- App/app/src/main/java/com/example/mycity/ui/map/MapFragment.kt
+++ App/app/src/main/java/com/example/mycity/ui/map/MapFragment.kt
@@ -1,11 +1,24 @@
 package com.example.mycity.ui.map
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.mycity.R
+import com.example.mycity.databinding.FragmentMapBinding
 
 /**
  * [Fragment] containing a map where events close to a location are shown in pins
  *
  * @author Pelayo Reguera García
  * */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(R.layout.fragment_map) {
+
+    private lateinit var binding: FragmentMapBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Use the static method from the class binding .bind(<<view>>) when the view is already
+        // inflated and we want again an instance of the class binding
+        binding = FragmentMapBinding.bind(view)
+    }
 }
