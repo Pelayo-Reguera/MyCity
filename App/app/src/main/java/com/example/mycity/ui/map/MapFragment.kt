7c6d542cbdc9077ed787833bb498d2ac400ec5cd
Pelayo-Reguera <UO282348@uniovi.es>
2024-03-28 21:58:39 +0100

    Copyright declarations

--- App/app/src/main/java/com/example/mycity/ui/map/MapFragment.kt
+++ App/app/src/main/java/com/example/mycity/ui/map/MapFragment.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024.
+ * MyCity © 2024 by Pelayo Reguera García is licensed under
+ * Attribution-NonCommercial-NoDerivatives 4.0 International.
+ *
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
 package com.example.mycity.ui.map
 
 import android.os.Bundle
