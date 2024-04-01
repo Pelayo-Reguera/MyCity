1391ec1dc08acff22bc1277c127065950a6cf2e8
Pelayo-Reguera <UO282348@uniovi.es>
2024-04-01 02:15:08 +0200

    Added the Copyright and the OnClickListener to cards

--- App/app/src/main/java/com/example/mycity/ui/explorer/ExplorerAdapter.kt
+++ App/app/src/main/java/com/example/mycity/ui/explorer/ExplorerAdapter.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024.
+ * MyCity © 2024 by Pelayo Reguera García is licensed under
+ * Attribution-NonCommercial-NoDerivatives 4.0 International.
+ *
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
 package com.example.mycity.ui.explorer
 
 import android.view.LayoutInflater
@@ -5,10 +13,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycity.R
 import com.example.mycity.logic.dataclasses.Event
 
+/**
+ * This class is in charge of generating components based on a layout which later will be
+ * used in a recycler view
+ * */
 class ExplorerAdapter(private val eventsList: List<Event>) :
     RecyclerView.Adapter<ExplorerAdapter.EventViewHolder>() {
 
@@ -32,7 +45,11 @@ class ExplorerAdapter(private val eventsList: List<Event>) :
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): EventViewHolder {
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.card_recycler_event, viewGroup, false)
+            .inflate(R.layout.card_event, viewGroup, false)
+
+        view.setOnClickListener { card ->
+            card.findNavController().navigate(R.id.to_eventInfoFragment)
+        }
 
         return EventViewHolder(view)
     }
