bee8595a9632a0618d7959714ebd36925d7deba4
Pelayo-Reguera <UO282348@uniovi.es>
2024-04-01 02:13:35 +0200

    New fragment to show a specific event
    - Created the XML
    - All the action names where shortened
    - New action to the fragment_event_info.xml created

---
+++ App/app/src/main/java/com/example/mycity/ui/explorer/event/EventInfoFragment.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024.
+ * MyCity © 2024 by Pelayo Reguera García is licensed under
+ * Attribution-NonCommercial-NoDerivatives 4.0 International.
+ *
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
+package com.example.mycity.ui.explorer.event
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.mycity.R
+import com.example.mycity.databinding.FragmentEventInfoBinding
+
+class EventInfoFragment : Fragment(R.layout.fragment_event_info) {
+
+    private lateinit var binding: FragmentEventInfoBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Use the static method from the class binding .bind(<<view>>) when the view is already
+        // inflated and we want again an instance of the class binding
+        binding = FragmentEventInfoBinding.bind(view)
+    }
+}
