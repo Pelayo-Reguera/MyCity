578325dc8ebfb2f2e333613c7ce2a53de2befeff
Pelayo-Reguera <UO282348@uniovi.es>
2024-07-06 23:19:30 +0200

    Creation of a secondary constructor for the User and Challenge, and new properties

--- App/app/src/main/java/my/city/logic/Data classes.kt
+++ App/app/src/main/java/my/city/logic/Data classes.kt
@@ -16,39 +16,55 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 /**
- * It represents all the information a challenge created for a specific event can contain
+ * It encapsulates all the information of a challenge created for a specific event
  *
  * @property name The name of the challenge
  * @property description
- * @property coinID The ID of a specific coin
+ * @property coinID The ID of a specific coin with which it is rewarded
  * @property reward The amount of coins that this challenge gives to whoever completes it
  *
  * @author Pelayo Reguera García
  * */
-data class Challenge(val name: String, val description: String, val coinID: String, val reward: Int)
+data class Challenge(var reward: Int = 0) {
+
+    lateinit var name: String
+    lateinit var description: String
+    lateinit var coinID: String
+
+    constructor(name: String, description: String, coinID: String, reward: Int = 0) : this(reward) {
+        this.name = name
+        this.description = description
+        this.coinID = coinID
+    }
+}
 
 /**
- * It represents all the information an event created by an organizer can contain
+ * It encapsulates all the information of an Event created by an organizer
  *
- * @property name The name of the event
+ * @property id The unique ID of the event in the database
+ * @property name The name of the Event
  * @property eventDrawables [MutableList] of drawables of the Event
  * @property eventImgURIs [MutableList] with paths to each image of the Event
- * @property organizers [MutableList] with the name of the users who organize this Event
+ * @property organizers [MutableList] with the users' names who organize this Event
  * @property description Details of the Event
  * @property challenges [MutableList] containing all activities that attendees can do during the event
  * @property location [GeoPoint] of the place where the event is held
- * @property street The address of the Event
+ * @property street The address of the  in text format
  * @property startEvent [Timestamp] representing the date and hour when the event begins.
  * The secondary constructor automatically converts from [LocalDateTime] to [Timestamp]
  * @property endEvent [Timestamp] representing the date and hour when the event ends.
  * The secondary constructor automatically converts from [LocalDateTime] to [Timestamp]
- * @property guests [MutableList] containing all the users that will attend this event
+ * @property guestsCapacity The maximum number of people that can attend to the Event
+ * @property guestsNJoined The current number of people joined to the Event
+ * @property guestsUserNames [MutableList] containing all the users that will attend this event
  *
  * @author Pelayo Reguera García
  * */
 data class Event(
     var eventImgURIs: MutableList<String> = mutableListOf(),
     var organizers: MutableList<String> = mutableListOf(),
+    var guestsCapacity: Int = 10,
+    var guestsNJoined: Int = 0,
 ) {
 
     @Exclude
@@ -73,7 +89,7 @@ data class Event(
     lateinit var endEvent: Timestamp
 
     @Exclude
-    var guests: MutableList<User> = mutableListOf()
+    var guestsUserNames: MutableList<String> = mutableListOf()
         @Exclude get
         @Exclude set
 
@@ -88,9 +104,11 @@ data class Event(
         street: String,
         startEvent: LocalDateTime,
         endEvent: LocalDateTime,
-        guests: MutableList<User>,
+        guestsUserNames: MutableList<String>,
+        guestsCapacity: Int = 10,
+        guestsNJoined: Int = 0,
         id: String = "", //It's the last one to avoid change all the calls to this constructor
-    ) : this(eventImgURIs, organizers) {
+    ) : this(eventImgURIs, organizers, guestsCapacity, guestsNJoined) {
         this.id = id
         this.name = name
         this.eventDrawables = eventDrawables
@@ -104,9 +122,14 @@ data class Event(
 
         instant = endEvent.atZone(ZoneId.systemDefault()).toInstant()
         this.endEvent = Timestamp(instant.epochSecond, instant.nano)
-        this.guests = guests
+        this.guestsUserNames = guestsUserNames
     }
 
+    /**
+     * It creates an exact copy of the Event
+     *
+     * @return An [Event] object
+     * */
     fun copy(): Event {
         return Event(
             name,
@@ -117,42 +140,84 @@ data class Event(
             challenges,
             GeoPoint(location.latitude, location.longitude),
             street,
-            startEvent.toDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
-            endEvent.toDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
-            guests,
+            getStartLocalDateTime(),
+            getEndLocalDateTime(),
+            guestsUserNames,
+            guestsCapacity,
+            guestsNJoined,
             id
         )
     }
 
-    fun startLocalDateTime(): LocalDateTime {
+    /**
+     * Transforms the [startEvent] in [Timestamp] format to [LocalDateTime] format
+     *
+     * @return [LocalDateTime] object
+     * */
+    fun getStartLocalDateTime(): LocalDateTime {
         return startEvent.toDate().toInstant().atZone(ZoneId.systemDefault())
             .toLocalDateTime()
     }
 
-    fun endLocalDateTime(): LocalDateTime {
+    /**
+     * Transforms the [endEvent] in [Timestamp] format to [LocalDateTime] format
+     *
+     * @return [LocalDateTime] object
+     * */
+    fun getEndLocalDateTime(): LocalDateTime {
         return endEvent.toDate().toInstant().atZone(ZoneId.systemDefault())
             .toLocalDateTime()
     }
 }
 
 /**
- * It represents an User with all the information attached to it
+ * It encapsulates all the information of an User (normally the one using the app)
  *
- * @property isAnonymous
- * @property userName
+ * @property name
  * @property email
- * @property photoUrl
- * @property likedEvents A [MutableList] containing all the events that the user like or is register
- * as an attendant
- * @property coins A [MutableList] containing the quantities of each coin type in the user's possession
+ * @property location The default location set when the User created its account
+ * @property birthdate
+ * @property gender
+ * @property friends [MutableList] of pair values with the usernames of friends as key
+ * and their profile photo as a [Drawable]
+ * @property createdEvents
+ * @property likedEvents [MutableList] containing all the events that the user like
+ * @property joinedEvents [MutableList] containing all the events that the user is registered
+ * @property coins [MutableList] containing the quantities of each coin type the user has
  *
  * @author Pelayo Reguera García
  * */
 data class User(
-    val userName: String,
-    val email: String,
-    val photoUrl: String,
-    val likedEvents: MutableList<Event>,
-    val coins: MutableMap<String, Int>,
-    val isAnonymous: Boolean = false,
-)
+    val friends: MutableList<Pair<String, Drawable>> = mutableListOf(),
+    val createdEvents: MutableList<Event> = mutableListOf(),
+    val likedEvents: MutableList<Event> = mutableListOf(),
+    val joinedEvents: MutableList<Event> = mutableListOf(),
+    val coins: MutableMap<String, Int> = mutableMapOf(),
+    var isAnonymous: Boolean = false,
+) {
+
+    var name: String = ""
+    var email: String = ""
+    var location: GeoPoint = GeoPoint(0.0, 0.0)
+    var birthdate: Timestamp = Timestamp.now()
+    var gender: String = ""
+
+    /**
+     * It is for when it is needed to create an User object manually
+     * */
+    constructor(
+        name: String,
+        email: String,
+        location: com.tomtom.sdk.location.GeoPoint,
+        friends: MutableList<Pair<String, Drawable>> = mutableListOf(),
+        createdEvents: MutableList<Event> = mutableListOf(),
+        likedEvents: MutableList<Event> = mutableListOf(),
+        joinedEvents: MutableList<Event> = mutableListOf(),
+        coins: MutableMap<String, Int> = mutableMapOf(),
+        isAnonymous: Boolean = false,
+    ) : this(friends, createdEvents, likedEvents, joinedEvents, coins, isAnonymous) {
+        this.name = name
+        this.email = email
+        this.location = GeoPoint(location.latitude, location.longitude)
+    }
+}
